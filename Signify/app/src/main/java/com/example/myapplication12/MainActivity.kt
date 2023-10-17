5aca9e577412b25caaab970a880920452cc5c51a
faheem4531 <faheem4531@gmail.com>
2023-10-17 23:51:03 +0500

    add date peeker feature

--- Signify/app/src/main/java/com/example/myapplication12/MainActivity.kt
+++ Signify/app/src/main/java/com/example/myapplication12/MainActivity.kt
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
+import android.widget.DatePicker
 import android.widget.RadioButton
+import android.widget.Switch
 import android.widget.TextView
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.Period
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun signUpUser(view: View) {
@@ -24,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         var cbCric =      findViewById<CheckBox>     (R.id.cbCricket);
         var cbFootball =  findViewById<CheckBox>     (R.id.cbFootball);
         var cbHockey =    findViewById<CheckBox>     (R.id.cbHockey);
-        var birth =       findViewById<TextView>     (R.id.birth);
+        var dateView =    findViewById<DatePicker>   (R.id.datePiker);
         var city =        findViewById<TextView>     (R.id.city);
         var country =     findViewById<TextView>     (R.id.country);
         var skillBegin =  findViewById<RadioButton>  (R.id.begin);
@@ -37,18 +44,33 @@ class MainActivity : AppCompatActivity() {
         var _mail =      email.text.toString();
         var _password =  password.text.toString();
         var _confrmPas = confirmPass.text.toString();
-        var _birth =     birth.text.toString();
         var _city =      city.text.toString();
         var _country =   country.text.toString();
-        var _gender=    "Gender: ";
+        var _gender =   "Gender: ";
+
+//       date of birth check
+        var date = dateView.dayOfMonth
+        var mon = dateView.month+1;
+        var year = dateView.year
+
+        var user_date = date.toString() +"/" + mon.toString() +"/" + year.toString();
+
+        val currentDate = LocalDate.now().toString()
+
+        output.setText(currentDate);
 
 
+
+
+
+
+//        Gender check
         if (female.isChecked)
             _gender =_gender+ " Female"
         else if (male.isChecked)
             _gender = _gender + " Male"
 
-
+//           Sports check
         var hobbies = "";
         if ( cbCric.isChecked )
             hobbies = hobbies + " Cricket";
@@ -60,6 +82,8 @@ class MainActivity : AppCompatActivity() {
             hobbies = hobbies + " Football" ;
 
 
+
+//             Skills check
         var skills = "";
         if (skillBegin.isChecked)
             skills = "beginner."
@@ -68,28 +92,30 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        if (_name.length<1 || (_password.length<1) || (_birth.length<1) || (_city.length<1) || (_country.length<1) || (_mail.length<1))
-            output.setText("Error! Fill above all fields");
-        else if (_password != _confrmPas )
-            output.setText("Error! Password not match");
-        else if (!female.isChecked && !male.isChecked)
-            output.setText("Select your gender");
-        else if (hobbies.length<1)
-            output.setText("Select atleast one Sport");
-        else if (!skillBegin.isChecked && !skillPro.isChecked)
-            output.setText("Error! Chose your skill level");
-        else if (!agrement.isChecked )
-            output.setText("Please check the terms and condition");
-        else
-            output.setText(
-                "Welcome " + _name +",\n "
-                + _mail+",\n"
-                +_gender + ",\n"
-                + "born: "+_birth+",\n"
-                +_city+", "+_country+".\n"
-                +"sports " + hobbies + ",\n"
-                +"Skills " +skills
-            );
+//        if (_name.length<1 || (_password.length<1) || (_city.length<1) || (_country.length<1) || (_mail.length<1))
+//            output.setText("Error! Fill above all fields");
+//        else if(true)
+//            output.setText("Error! You are under 18");
+//        else if (_password != _confrmPas );
+//            output.setText("Error! Password not match");
+//        else if (!female.isChecked && !male.isChecked)
+//            output.setText("Select your gender");
+//        else if (hobbies.length<1)
+//            output.setText("Select atleast one Sport");
+//        else if (!skillBegin.isChecked && !skillPro.isChecked)
+//            output.setText("Error! Chose your skill level");
+//        else if (!agrement.isChecked )
+//            output.setText("Please check the terms and condition");
+//        else
+//            output.setText(
+//                "Welcome " + _name +",\n "
+//                + _mail+",\n"
+//                +_gender + ",\n"
+//                + "born: "+_dateView+",\n"
+//                +_city+", "+_country+".\n"
+//                +"sports " + hobbies + ",\n"
+//                +"Skills " +skills
+//            );
 
     }
 
@@ -128,5 +154,20 @@ class MainActivity : AppCompatActivity() {
         skillPro.isChecked = false
         agrement.isChecked = false
         output.text = null
+    }
+
+    fun handleToggle (view: View){
+        var switch = findViewById<Switch>  (R.id.switchView);
+        var dateView =findViewById<DatePicker>  (R.id.datePiker);
+        if (!switch.isChecked){
+            dateView.calendarViewShown = true
+            dateView.spinnersShown = false
+            dateView.visibility = View.VISIBLE
+        }
+        else{
+            dateView.calendarViewShown = false
+            dateView.spinnersShown = true
+            dateView.visibility = View.VISIBLE
+        }
     }
 }
