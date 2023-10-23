7ac90a372f89484e7404003af5def15b548b9862
faheem4531 <faheem4531@gmail.com>
2023-10-24 00:20:33 +0500

    add search by google feature in home

--- Signify/app/src/main/java/com/example/myapplication12/Home.kt
+++ Signify/app/src/main/java/com/example/myapplication12/Home.kt
@@ -1,12 +1,25 @@
 package com.example.myapplication12
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import com.example.Signify.R
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun chrome(view: View) {
+        var searchText = findViewById<EditText>  (R.id.search );
+        var _searchText = searchText.text.toString();
+
+        val Search = Intent(Intent.ACTION_VIEW);
+        Search.setData(Uri.parse("https://www.google.com/search?q="+_searchText));
+        startActivity(Search);
     }
 }
