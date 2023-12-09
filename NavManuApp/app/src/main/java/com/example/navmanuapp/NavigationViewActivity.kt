094b47d3f0ad5d5236f382ace5f96e3caad43109
faheem4531 <faheem4531@gmail.com>
2023-12-10 02:39:39 +0500

    fetch data from real time db

--- NavManuApp/app/src/main/java/com/example/navmanuapp/NavigationViewActivity.kt
+++ NavManuApp/app/src/main/java/com/example/navmanuapp/NavigationViewActivity.kt
@@ -3,6 +3,8 @@ package com.example.navmanuapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,13 +18,20 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.navmanuapp.databinding.ActivityNavigationViewBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class NavigationViewActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityNavigationViewBinding
+    private lateinit var currentUser: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityNavigationViewBinding.inflate(layoutInflater)
@@ -32,9 +41,8 @@ class NavigationViewActivity : AppCompatActivity() {
 
         var auth: FirebaseAuth=FirebaseAuth.getInstance()
 
-
-
-
+        //function call to get username and email to show on header
+        displayUserInfo(auth)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -89,6 +97,40 @@ class NavigationViewActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun displayUserInfo(auth: FirebaseAuth) {
+
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val dbRef = FirebaseDatabase.getInstance().getReference("Signup").child(userId.toString())
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val dbName = snapshot.child("name").getValue(String::class.java)
+                    val dbEmail = snapshot.child("email").getValue(String::class.java)
+
+                    val headerView = binding.navView.getHeaderView(0)
+                    val userNameTextView: TextView = headerView.findViewById(R.id.userName)
+                    val userEmailTextView: TextView = headerView.findViewById(R.id.userEmail)
+
+                    userNameTextView.text = dbName
+                    userEmailTextView.text = dbEmail
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle an error in fetching the data
+                val errorMessage = "Error: ${error.message}" // Get the error message from DatabaseError
+
+                // Use applicationContext or pass a valid context to Toast.makeText()
+                Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+    }
 
 
     override fun onSupportNavigateUp(): Boolean {
