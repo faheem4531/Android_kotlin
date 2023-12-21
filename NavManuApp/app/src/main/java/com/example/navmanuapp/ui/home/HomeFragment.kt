c93a2f6adf70e0633d6eba23eb2ca33ca81045e6
faheem4531 <faheem4531@gmail.com>
2023-12-22 02:51:00 +0500

    apply array adapter

--- NavManuApp/app/src/main/java/com/example/navmanuapp/ui/home/HomeFragment.kt
+++ NavManuApp/app/src/main/java/com/example/navmanuapp/ui/home/HomeFragment.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.navmanuapp.R
 import com.example.navmanuapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    //declearation
+    private lateinit var listView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+    private val dataList = mutableListOf("")
+
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +39,30 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        //Add hobby
+        val btnAddHoby = root.findViewById<Button>(R.id.btnAddHobby)
+        val hobby = root.findViewById<EditText>(R.id.addHobby)
+
+        btnAddHoby.setOnClickListener {
+            val newHobby = hobby.text.toString().trim()
+            if (newHobby.isNotEmpty()) {
+                dataList.add(newHobby)
+                adapter.notifyDataSetChanged() // Update the ListView
+                hobby.text.clear()
+            }
+            else{
+                Toast.makeText(requireContext(), "Enter you hobby", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        //Apply Adapter
+        listView = root.findViewById(R.id.hobbiesList) // Using the ID from your XML layout
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, dataList)
+        listView.adapter = adapter
+
+
+
         return root
     }
 
@@ -40,3 +71,5 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
+
+
