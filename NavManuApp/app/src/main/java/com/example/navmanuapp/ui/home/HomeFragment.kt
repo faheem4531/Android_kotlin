3727e8b75c2cf5ef22e39b611cf9bcac48da4532
faheem4531 <faheem4531@gmail.com>
2023-12-22 22:08:53 +0500

    fetch and store hobies in firebase

--- NavManuApp/app/src/main/java/com/example/navmanuapp/ui/home/HomeFragment.kt
+++ NavManuApp/app/src/main/java/com/example/navmanuapp/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.navmanuapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +16,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.navmanuapp.R
 import com.example.navmanuapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
 
@@ -25,6 +32,8 @@ class HomeFragment : Fragment() {
     private lateinit var adapter: ArrayAdapter<String>
     private val dataList = mutableListOf("")
 
+    private lateinit var dbRef : DatabaseReference
+    private var key : Int = 0
 
     private val binding get() = _binding!!
 
@@ -43,9 +52,28 @@ class HomeFragment : Fragment() {
         val btnAddHoby = root.findViewById<Button>(R.id.btnAddHobby)
         val hobby = root.findViewById<EditText>(R.id.addHobby)
 
+        val userId=FirebaseAuth.getInstance().currentUser?.uid
+        dbRef=FirebaseDatabase.getInstance().getReference("Signup").child(userId.toString()).child("hobbies")
+
+
         btnAddHoby.setOnClickListener {
             val newHobby = hobby.text.toString().trim()
             if (newHobby.isNotEmpty()) {
+
+                dbRef.push().setValue(newHobby)
+                    .addOnSuccessListener {
+                        Toast.makeText(requireContext(), "sucess", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(requireContext(), "fail", Toast.LENGTH_SHORT).show()
+                    }
+
+
+
+
+
+
+
                 dataList.add(newHobby)
                 adapter.notifyDataSetChanged() // Update the ListView
                 hobby.text.clear()
@@ -54,6 +82,29 @@ class HomeFragment : Fragment() {
                 Toast.makeText(requireContext(), "Enter you hobby", Toast.LENGTH_SHORT).show()
             }
         }
+
+        //fetch hobbies from db and store in an array
+       if (userId != null){
+
+           dbRef.addValueEventListener(object : ValueEventListener {
+               override fun onDataChange(snapshot: DataSnapshot) {
+                   dataList.clear()
+                   for (hobby in snapshot.children){
+                       val currentHobby = hobby.getValue(String::class.java)
+                       if (currentHobby != null){
+                           dataList.add(currentHobby.toString())
+                       }
+                   }
+                   adapter.notifyDataSetChanged()
+               }
+
+               override fun onCancelled(error: DatabaseError) {
+                   // Handle onCancelled
+                   Log.e("Firebase", "Error: ${error.message}")
+               }
+
+           })
+       }
 
 
         //Apply Adapter
